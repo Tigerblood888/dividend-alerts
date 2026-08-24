177e4ccd8914ef9683a25bc6987b1824007f5a99
Tigerblood888 <homeostasis888@gmail.com>
2026-08-24 16:14:32 -0600

    Refactor Telegram bot token retrieval method

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 
 # --- CONFIGURATION ENGINE ---
 FMP_API_KEY = "OefKADASIS81FNIXFvv7KeaC8xjUekRo"
-TELEGRAM_BOT_TOKEN = "8852179205:AAHYkHsEdtawzkBLu3Nr8hDEXjgNajGAJL0"
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = "8639836189"
 
 MY_STOCKS = ["MPLX", "SPCX", "CIEN", "CRWV", "SMCI", "FSK", "RWAY", "QFIN", "HTGC", "BXSL", "MU", "NOW", "TSM", "NVDA", "TSLA", "PLTR", "AGNC", "ARCC", "ET", "HRZN", "MELI", "MRVL", "PSEC", "TRIN", "WES"]
